f6f95faed00d6c4a37c3f9c0b032c0b0c16d7a20
Bhavya Chugh <bhavvyachugh@gmail.com>
2022-08-17 00:02:48 +0530

    feat: :sparkles: start todo

---
+++ src/layout/nav/index.jsx
@@ -0,0 +1,20 @@
+const Nav = () => {
+  return (
+    <header>
+      <nav>
+        <div>
+          <h1>TODO</h1>
+        </div>
+        <div>
+          <ul>
+            <li>Login</li>
+            <li>Sign up</li>
+            <li>Logout</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
